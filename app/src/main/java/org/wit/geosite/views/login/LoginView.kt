1c2bc920b2e23f7634253cf41214ccc69f3b75c3
HEIMDALL\Ivand <20091388@mail.wit.ie>
2022-04-11 22:17:45 +0100

    Login bug fix. Revision to only allow log in of pre-registered email accounts, prevents sign up and input by unauthorised users.

--- app/src/main/java/org/wit/geosite/views/login/LoginView.kt
+++ app/src/main/java/org/wit/geosite/views/login/LoginView.kt
@@ -1,31 +1,14 @@
 package org.wit.geosite.views.login
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.MutableLiveData
-import com.google.android.gms.auth.api.signin.*
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
-import org.wit.geosite.R
 import org.wit.geosite.databinding.ActivityLoginBinding
-import org.wit.geosite.views.geositelist.GeositeListView
-import timber.log.Timber
 
 class LoginView : AppCompatActivity(){
-    private val RC_SIGN_IN: Int = 0
-    lateinit var googleSignInClient: GoogleSignInClient
     lateinit var presenter: LoginPresenter
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var loginIntentLauncher : ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -35,17 +18,6 @@ class LoginView : AppCompatActivity(){
         setContentView(binding.root)
 
         binding.progressBar.visibility = View.GONE
-
-        binding.signUp.setOnClickListener {
-            val email = binding.email.text.toString()
-            val password = binding.password.text.toString()
-            if (email == "" || password == "") {
-                showSnackBar("please provide email and password")
-            }
-            else {
-                presenter.doSignUp(email,password)
-            }
-        }
 
         binding.logIn.setOnClickListener {
             val email = binding.email.text.toString()
@@ -57,22 +29,11 @@ class LoginView : AppCompatActivity(){
                 presenter.doLogin(email,password)
             }
         }
-
-        binding.googleSignIn.setSize(SignInButton.SIZE_WIDE)
-        binding.googleSignIn.setColorScheme(0)
-
-        createRequest()
-        
-        binding.googleSignIn.setOnClickListener{
-            signIn()
-        }    
     }
-
     fun showSnackBar(message: CharSequence){
         Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
             .show()
     }
-
     fun showProgress() {
         binding.progressBar.visibility = View.VISIBLE
     }
@@ -80,37 +41,4 @@ class LoginView : AppCompatActivity(){
     fun hideProgress() {
         binding.progressBar.visibility = View.GONE
     }
-
-    fun createRequest(){
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-    }
-
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = task.getResult(ApiException::class.java)!!
-                presenter.firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                showSnackBar("Login Failed")
-            }
-        }
-    }
-
-
-
-
-
 }
